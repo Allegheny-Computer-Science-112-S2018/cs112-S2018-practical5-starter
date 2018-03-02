bf8be058ead49a42a7fc2eebdbfb241fc8389146
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-03-02 12:31:23 -0500

    Adding the first version of the QuickSort file.

---
+++ src/main/java/practicalfive/sort/QuickSort.java
@@ -0,0 +1,49 @@
+package practicalfive.sort;
+
+import java.util.Arrays;
+
+/**
+ * A wrapper class to call a Dual-Pivot Quicksort provided by Arrays.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+// TODO: Make the correct class declaration
+public class QuickSort {
+
+  /** Define the name of this sorting algorithm. */
+  public QuickSort() {
+    // TODO: Define the name of the sorting algorithm
+  }
+
+  /** QuickSort sort of a provided char[] array.
+   *
+   * <p>Please refer to this site:
+   * https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html#sort(char[])
+   * for more details about how this method works.
+   *
+   * @param source the source array that will be copied and sorted
+   * @return the copied and sorted array in a nondecreasing order
+   */
+  public char[] sort(char[] source) {
+    // TODO: Call the correct sort method provided by Arrays.sort
+  }
+
+  /** QuickSort sort of a provided int[] array.
+   *
+   * <p>Please refer to this site:
+   * https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html#sort(int[])
+   * for more details about how this method works.
+   *
+   * @param source the source array that will be copied and sorted
+   * @return the copied and sorted array in a nondecreasing order
+   */
+  public int[] sort(int[] source) {
+    // TODO: Call the correct sort method provided by Arrays.sort
+  }
+
+  /** Demonstrate the use of the QuickSort algorithm. */
+  public static void main(String[] args) {
+    // TODO: Add a demonstration for the QuickSort method provided
+  }
+}
